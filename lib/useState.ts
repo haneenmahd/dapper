27ed6846ce512ffe4b934b2a86075a5c17abe4c2
Haneen Mahdin <haneenmahdin@gmail.com>
2022-07-20 20:03:37 +0530

    🥸: return the state object instead of state value

--- lib/useState.ts
+++ lib/useState.ts
@@ -1,9 +1,9 @@
 import State from "./State";
 
-function useState<T>(value: T): StateObject<T> {
+function useState<T>(value: T): StateDestructor<T> {
   const state = new State(value);
 
-  const getter = () => state.get();
+  const getter = () => state;
   const setter = (newValue: T) => state.set(newValue);
 
   return [getter, setter];
