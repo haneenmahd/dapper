1e3e5d74c22db34f4bf1196b81790be3efb61223
Haneen Mahdin <haneenmahdin@gmail.com>
2022-07-20 19:59:22 +0530

    🥸: add onChange handler into the State class

--- lib/State.ts
+++ lib/State.ts
@@ -1,12 +1,18 @@
-class State<T = any> {
+class State<T> implements StateObject<T> {
   value: T;
+  onChange?: (newValue: T) => void;
 
-  constructor(value: T) {
+  constructor(value: T, onChange?: (newValue: T) => void) {
     this.value = value;
+    this.onChange = onChange;
   }
 
   set(newValue: T) {
     this.value = newValue;
+
+    if (this.onChange) {
+      this.onChange(this.value);
+    }
   }
 
   get() {
