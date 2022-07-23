69e3868252ab07cdbd0ca3dc614037dbc3524eec
Haneen Mahdin <haneenmahdin@gmail.com>
2022-07-23 19:40:23 +0530

    🧐: add docs for `registerEffect`

--- lib/registerEffect.ts
+++ lib/registerEffect.ts
@@ -1,5 +1,19 @@
+/**
+ * Register an effect for the state object specified in the argument.
+ * This function also supports specifying multiple state objects as arguments
+ * when you want to have a common state for multiple state objects.
+ *
+ * ```ts
+ * createEffect((newValue) => {
+ *  // callback when the state is set
+ * }, state);
+ * ```
+ *
+ * @param callback {StateCallback<T>}
+ * @param objects {StateObject<T> | StateObject<T>[]}
+ */
 function registerEffect<T>(
-  callback: (newValue: T) => void,
+  callback: StateCallback<T>,
   objects: StateObject<T> | StateObject<T>[]
 ) {
   if (Array.isArray(objects)) {
