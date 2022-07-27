3aab9c22d2974a6d6e552b1d4ebc9cf4f6d82388
Haneen Mahdin <haneenmahdin@gmail.com>
2022-07-27 07:32:05 +0530

    🥸: return an instance of the state in `createState()`

--- lib/createState.ts
+++ lib/createState.ts
@@ -3,12 +3,13 @@ import type { StateDestructor } from "./types";
 
 /**
  * Creates a new state object and return an array with it's first value
- * as the getter and second value as an setter. Instead of creating a new
- * new instance of `State`, this function should be used.
+ * as the getter and second value as an setter and the third as the instance
+ * of the state. Instead of creating a new new instance of `State`, this
+ * function should be used.
  *
  *
  * ```ts
- * const [checked, setChecked] = createState(false);
+ * const [checked, setChecked, checkedObject] = createState(false);
  *
  * // To retrieve the value from the state
  * let value = checked();
@@ -27,7 +28,7 @@ function createState<T>(value: T): StateDestructor<T> {
   const getter = state;
   const setter = (newValue: T) => state.set(newValue);
 
-  return [getter, setter];
+  return [getter, setter, state];
 }
 
 export default createState;
