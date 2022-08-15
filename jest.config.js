6167433f4538ccaab62b3624f3c02ccd6d0e1176
Haneen Mahdin <haneenmahdin@gmail.com>
2022-08-15 14:58:24 +0530

    🤠: format

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 export default {
-  preset: "ts-jest",
-  transform: {},
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+	preset: 'ts-jest',
+	transform: {},
+	transformIgnorePatterns: ['<rootDir>/node_modules/']
 };
